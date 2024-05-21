35147a7cdf45093f182ad7f78a7b81170f6f57fb
woosung <monnalija@naver.com>
2024-05-21 21:37:39 +0900

    Etc: next 초기 세팅

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 't1.daumcdn.net',
+        port: '',
+        pathname: '/localimg/localimages/07/mapapidoc/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
